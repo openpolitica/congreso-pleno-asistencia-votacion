4d1f6951d740f1d98918a270b958932e3ba1a158
Jorge Esteban Quilcate Otoya <quilcate.jorge@gmail.com>
2022-09-25 09:59:24 +0100

    update plenos

--- src/main/java/op/congreso/pleno/app/RetryProcessRegistroPleno.java
+++ src/main/java/op/congreso/pleno/app/RetryProcessRegistroPleno.java
@@ -3,6 +3,7 @@ package op.congreso.pleno.app;
 import static op.congreso.pleno.textract.TextractRegistroPleno.processLines;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import op.congreso.pleno.textract.TextractRegistroPleno;
 
@@ -13,6 +14,9 @@ public class RetryProcessRegistroPleno {
     var pleno = TextractRegistroPleno.plenoToRetry(of);
     var lines = TextractRegistroPleno.retryProcessRegistroPleno(of);
     var regPleno = processLines(pleno, lines);
+    Files.writeString(Path.of("pr-title.txt"), pleno.prTitle());
+    Files.writeString(Path.of("pr-branch.txt"), pleno.prBranchName());
+    Files.writeString(Path.of("pr-content.txt"), pleno.prContent());
     SaveRegistroPlenoToCsv.save(regPleno);
   }
 }
